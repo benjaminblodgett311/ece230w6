63403c898057870e2f325a93c2e5cc4ccdcb7f45
Your Name <hannahszychulda@u.boisestate.edu>
2024-09-24 09:51:36 -0600

    changes

--- ones_compliment.v
+++ ones_compliment.v
@@ -1,11 +1,18 @@
 module onescompliment(
     input A0, A1, A2, A3, B0, B1, B2, B3, 
-    #or can we do input [3:0] A, [3:0] B
+   
     output Ones0, Ones1, Ones2, Ones3
 );
 
-assign Y = A ^ B;
-assign Borrow = (~A) & B;
+assign Ones0 = A0 ^ B0;
+assign Ones1 = A1 ^ B1; 
+assign Ones2 = A2 ^ B2;
+assign Ones3 = A3 ^ B3; 
+
+
+
+
+
 
 
 endmodule
---
+++ twos_compliment.v
@@ -0,0 +1,12 @@
+module twoscompliment(
+    input A0, A1, A2, A3, B0, B1, B2, B3, 
+   
+    output Twos0, Twos1, Twos2, Twos3, Twos4, Twos5, Twos6, Twos7 
+);
+
+
+
+
+
+
+endmodule
